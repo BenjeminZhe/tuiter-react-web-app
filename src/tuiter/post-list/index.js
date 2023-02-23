40f6b579de6b5608d29b1202335cf1c8ad1fccfb
Zhe Xi <zhexi@ZhedeMacBook-Pro.local>
2023-02-23 02:17:43 -0800

    a6 React.js sp22

--- src/tuiter/post-list/index.js
+++ src/tuiter/post-list/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import postsArray from './posts.json';
-import PostItem
-  from "./post-item";
+import PostItem from "./post-item";
 
 const PostList = () => {
   console.log(postsArray);
